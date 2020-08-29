77a39aa9fb73b56470062fbc76e0d322befec211
kouki <kouworld0123@gmail.com>
2020-08-29 19:22:14 +0900

    1-1で通信するnsを作るスクリプト

---
+++ delete_all_namespaces.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+for ns in $(ip netns list | awk '{print $1}'); do
+    sudo ip netns delete $ns
+done
---
+++ one-to-one.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+sudo ip netns add ns1
+sudo ip netns add ns2
+
+sudo ip link add ns1-veth0 type veth peer name ns2-veth0
+
+sudo ip link set ns1-veth0 netns ns1
+sudo ip link set ns2-veth0 netns ns2
+
+sudo ip netns exec ns1 ip address add 192.0.2.1/24 dev ns1-veth0
+sudo ip netns exec ns2 ip address add 192.0.2.2/24 dev ns2-veth0
+
+sudo ip netns exec ns1 ip link set ns1-veth0 up
+sudo ip netns exec ns2 ip link set ns2-veth0 up
